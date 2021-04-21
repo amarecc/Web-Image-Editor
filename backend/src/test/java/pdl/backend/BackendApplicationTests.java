1dbf8416b072330cb0e4d28caa1b70c06817505b
Alexis Marec <alexis.marec@etu.u-bordeaux.fr>
2021-04-21 16:04:55 +0200

    add h2 db for test

--- backend/src/test/java/pdl/backend/BackendApplicationTests.java
+++ backend/src/test/java/pdl/backend/BackendApplicationTests.java
@@ -7,19 +7,21 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.core.annotation.Order;
 import pdl.backend.web.controller.ImageController;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @TestMethodOrder(OrderAnnotation.class)
 class BackendApplicationTests {
 
 	@Autowired
 	private ImageController imageController;
 
-	/*@Test
+	@Test
 	@Order(1)
 	void contextLoads() {
 		assertThat(imageController).isNotNull();
-	}*/
+	}
 }
--- backend/src/test/java/pdl/backend/ImageControllerTests.java
+++ backend/src/test/java/pdl/backend/ImageControllerTests.java
@@ -18,17 +18,19 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class)
 public class ImageControllerTests {
 
-	/*@Autowired
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
@@ -47,7 +49,12 @@ public class ImageControllerTests {
 			.andExpect(status().isNotFound());
 	}
 
-
+	/*@Test
+	@Order(3)
+	public void deleteImageShouldReturnSuccess() throws Exception {
+		mockMvc.perform(delete("/images/0"))
+			.andExpect(status().isOk());
+	}*/
 
 	@Test
 	@Order(4)
@@ -65,5 +72,5 @@ public class ImageControllerTests {
 
 		mockMvc.perform(MockMvcRequestBuilders.multipart("/images").file(someText).contentType(MediaType.TEXT_PLAIN))
 			.andExpect(status().isUnsupportedMediaType());
-	}*/
+	}
 }
